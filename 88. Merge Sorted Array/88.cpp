925102148b905ab3dc25e404d3acf05983cb8d6a
SequentialEntropy <asahi79289@gmail.com>
2024-08-30 12:39:02 +0900

    88. add testcases

--- 88. Merge Sorted Array/88.cpp
+++ 88. Merge Sorted Array/88.cpp
@@ -4,74 +4,88 @@
 
 using namespace std;
 
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        queue<int> q1;
+        queue<int> q2;
+
+        // for (int n1 : nums1)
+        for (int i = 0; i < m; i++)
+        {
+            q1.push(nums1[i]);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            q2.push(nums2[i]);
+        }
+
+        int index = 0;
+        while (q1.size() + q2.size() > 0)
+        {
+            if (q2.empty())
+            {
+                // cout << q1.front() << '\n';
+                nums1[index] = q1.front();
+                q1.pop();
+                index++;
+                continue;
+            }
+            if (q1.empty())
+            {
+                // cout << q2.front() << '\n';
+                nums1[index] = q2.front();
+                q2.pop();
+                index++;
+                continue;
+            }
+            if (q1.front() < q2.front())
+            {
+                // cout << q1.front() << '\n';
+                nums1[index] = q1.front();
+                q1.pop();
+                index++;
+            }
+            else
+            {
+                // cout << q2.front() << '\n';
+                nums1[index] = q2.front();
+                q2.pop();
+                index++;
+            }
+        }
+    }
+};
+
 int main() {
+    cout << "Hello LeetCode!\n";
+    Solution solution;
+
+    // Declare arguments
+    vector<int> nums1;
+    vector<int> nums2;
+    int m;
+    int n;
+
     // Test case 1
-    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
-    vector<int> nums2 = {2, 5, 6};
+    nums1 = {1, 2, 3, 0, 0, 0};
+    nums2 = {2, 5, 6};
 
     // Test case 2
-    vector<int> nums1 = {1};
-    vector<int> nums2 = {};
+    // nums1 = {1};
+    // nums2 = {};
 
     // Test case 3
-    vector<int> nums1 = {0};
-    vector<int> nums2 = {1};
+    // nums1 = {0};
+    // nums2 = {1};
+    
+    n = nums2.size();
+    m = nums1.size() - n;
 
-    int n = nums2.size();
-    int m = nums1.size() - n;
+    solution.merge(nums1, m, nums2, n);
 
-    queue<int> q1;
-    queue<int> q2;
-
-    // for (int n1 : nums1)
-    for (int i = 0; i < m; i++)
-    {
-        // cout << nums1[i] << '\n';
-        q1.push(nums1[i]);
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        q2.push(nums2[i]);
-    }
-
-    short index = 0;
-    while (q1.size() + q2.size() > 0)
-    {
-        if (q2.empty())
-        {
-            // cout << q1.front() << '\n';
-            nums1[index] = q1.front();
-            q1.pop();
-            index++;
-            continue;
-        }
-        if (q1.empty())
-        {
-            // cout << q2.front() << '\n';
-            nums1[index] = q2.front();
-            q2.pop();
-            index++;
-            continue;
-        }
-        if (q1.front() < q2.front())
-        {
-            // cout << q1.front() << '\n';
-            nums1[index] = q1.front();
-            q1.pop();
-            index++;
-        }
-        else
-        {
-            // cout << q2.front() << '\n';
-            nums1[index] = q2.front();
-            q2.pop();
-            index++;
-        }
-    }
-
-    for (int i : nums1)
-    {
-        cout << i << ' ';
+    for (int num: nums1) {
+        cout << num << ' ';
     }
 }
